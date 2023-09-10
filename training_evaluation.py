374cfaa17efd0254693b8ea6bac7e0574db3e79c
Edward Campbell <ecampbell@gts.tsc.uvigo.es>
2023-09-10 19:20:53 +0200

    server

--- training_evaluation.py
+++ training_evaluation.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     selected_GPU, device = select_gpu_with_most_free_memory()
     TASK_LIST = ["Reading-Task","Interview-Task" ]  # , "Reading-Task"
     CORPUS = "Androids-Corpus"
-    FEATURE_TYPE_LIST = ["melSpectrum", "rasta", "compare_lld","egemap_lld", "hubert_base", "wav2vec2_base"]
+    FEATURE_TYPE_LIST = ["melSpectrum", "rasta", "compare_lld","egemap_lld","hubert_base", "wav2vec2_base"] # 
 
     # feature_type = "melSpectrum"#"melSpectrum" compare_lld egemap_lld rasta hubert_base wav2vec2_base
     for feature_type in FEATURE_TYPE_LIST:
