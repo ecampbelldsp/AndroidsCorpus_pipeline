74cb58b1c08f01a1898d5318085d3dfea6f76062
Edward L. Campbell <77728779+ecampbelldsp@users.noreply.github.com>
2023-09-10 19:34:25 +0200

    main flow. With and without interviewer analysis

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 source ~/anaconda3/bin/activate pytorch1.13
 echo "Starting feature extraction"
 python feature_extraction.py /home/gts/projects/ecampbell/databases/
-echo "Starting training and evaluation pipeline"
+echo "Starting training and evaluation pipeline with interviewer in recordings"
 python training_evaluation.py -i
+echo "Starting training and evaluation pipeline without interviewer in recordings"
+python training_evaluation.py
