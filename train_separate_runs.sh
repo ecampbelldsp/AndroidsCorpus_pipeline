8e548a45230fe7ea8a706c2f720108205687e407
Edward Campbell <ecampbell@gts.tsc.uvigo.es>
2023-09-17 02:06:55 +0200

    runs 50

--- train_separate_runs.sh
+++ train_separate_runs.sh
@@ -1,3 +1,3 @@
-python training_evaluation_separate_runs.py -i -r 40
-python training_evaluation_separate_runs.py  -r 40
+python training_evaluation_separate_runs.py -i -r 50
+python training_evaluation_separate_runs.py  -r 50
 
